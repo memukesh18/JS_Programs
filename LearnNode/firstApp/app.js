075307834ab4738aff32a666f3935da4bda93c71
Mukesh K <me.mukesh18@gmail.com>
2020-10-05 15:31:11 +0530

    Added firstapp to LearnNode

---
+++ LearnNode/firstApp/app.js
@@ -0,0 +1,13 @@
+
+const EventEmitter = require('events');
+
+const Logger = require('./logger');
+const logger = new Logger();
+
+//Register a listener
+logger.on('messageLogged', (arg) => { //e, evemtArg
+    console.log('Listener called', arg);
+});
+
+logger.log('message');
+
